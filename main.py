a029e56da7d2da3a07e0ea195498934acbc77350
Carson Lau <carselau@gmail.com>
2022-06-09 21:09:20 -0400

    Load .env variables and send text with quote

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import os
 import requests
+from dotenv import load_dotenv
 from twilio.rest import Client
 
 '''
@@ -6,6 +8,13 @@ The Daily Ye is a simple daily SMS service that inspires you with daily insight 
 
 Why? "Because my life is dope, and I do dope shit" -Kanye West 
 '''
+
+# Get environment variables
+load_dotenv()
+TWILIO_SID = os.getenv("TWILIO_SID")
+TWILIO_API_KEY =  os.getenv("TWILIO_API_KEY")
+FROM_PHONE = os.getenv("FROM_PHONE")
+TO_PHONE = os.getenv("TO_PHONE")
 
 # Return a quote from the Kanye West REST API
 def get_quote() -> str:
@@ -15,8 +24,24 @@ def get_quote() -> str:
 
     return data["quote"]
 
+# Send message with returned quote
+def send_message(quote:str):
+
+    text_message = f'Today\'s Daily Ye:\n"{quote}"\n -Kanye West' 
+
+    client = Client(TWILIO_SID, TWILIO_API_KEY)
+
+    message = client.messages \
+                    .create(
+                        body = text_message,
+                        from_= FROM_PHONE,
+                        to= TO_PHONE
+                    )
+
+    print(message.sid)
+
 def main():
-    print(get_quote())
+    send_message(get_quote())
 
 if __name__ == "__main__":
     main()
